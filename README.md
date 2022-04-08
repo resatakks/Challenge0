2b796a27a9d95c72f881a53234431ab92b3e7379
tahos81 <tahirozpala@gmail.com>
2022-04-08 14:57:17 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# ITU Blockchain haftalık solidity challengeları
+
+## Challenge0
+
+### Yapılacaklar 
+
+1. Repo'yu indirin
+2. `npm install` 
+3. Challenge0.sol dosyasını spesifikasyona göre tamamlayın
+4. çözümünüzü test etmek için `npx hardhat test` komutunu kullanın
---
+++ contracts/Challenge0.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity >= 0.7.0 <0.9.0;
+
+contract Challenge0 {
+
+    /*
+        state variable'lar implement edilecek
+    */
+
+    // fonksiyonlar implement edilecek
+    constructor() {}
+
+    function setWithdrawAmount() {}
+
+    function setUserBalance() {}
+
+    function getUserBalance() {}
+
+    function checkWithdraw() {}
+
+}
