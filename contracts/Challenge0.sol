a9b3c8616fb767bc61d8a9bf5060f98bce795e9b
Resat Akkus <resatakkus10@gmail.com>
2022-04-10 10:07:59 +0300

    variables
    add state variables

--- contracts/Challenge0.sol
+++ contracts/Challenge0.sol
@@ -3,19 +3,19 @@
 pragma solidity >= 0.7.0 <0.9.0;
 
 contract Challenge0 {
+address private owner;
+uint256 userbalance;
+uint256 withdrawamount;
+bool public canWithdraw= false;
 
-    /*
-        state variable'lar implement edilecek
-    */
-
-    // fonksiyonlar implement edilecek
-    constructor() {}
+    constructor() public{}
 
     function setWithdrawAmount() {}
 
     function setUserBalance() {}
-
-    function getUserBalance() {}
+    
+function getUserBalance()external view returns (uint){
+    return address(this).balance;
 
     function checkWithdraw() {}
 
