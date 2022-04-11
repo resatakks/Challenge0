b18092fbdcbb155b8e1d94a89610e2ed1516f5c8
Resat Akkus <resatakkus10@gmail.com>
2022-04-11 08:55:12 +0300

    Update Challenge0.sol

--- contracts/Challenge0.sol
+++ contracts/Challenge0.sol
@@ -1,33 +1,37 @@
-// SPDX-License-Identifier: UNLICENSED
-
+// SPDX-License-Identifier: MIT
 pragma solidity >= 0.7.0 <0.9.0;
 
 contract Challenge0 {
-address public owner;
-uint256 userBalance;
-uint256 withdrawAmount;
-bool public canWithdraw= false;
+    address public owner;
+    uint256 userBalance;
+    uint256 withdrawAmountVariable;
+    bool public canWithdraw = false;
+  
+    constructor() {
+        owner = msg.sender;
+    }
 
-    constructor (){
-     function withdrawAmount(uint256 amount) onlyOwner payable public {
-         require(msg.value == amount);
-         require(amount <= getBalance());
-         msg.sender.transfer(amount);
-  }
+    modifier onlyOwner() {
+        require(owner == msg.sender, "You aren't the smart contact's owner");
+        _;
+    }
+
+    function getBalance() public returns(uint) {
+        
+    }
+
+    function withdrawAmount(uint256 amount) onlyOwner payable public {
+        require(msg.value == amount);
+        require(amount <= getBalance());
+        payable(msg.sender).transfer(amount);
     }
 
     function setUserBalance()external view {
-
+        
     }
     
-function getUserBalance()public returns (uint256){
-    return address(this).balance;
-
-    }
-    // function checkWithdraw() external view {
-
-
+    function getUserBalance()public view returns (uint256){
+        return address(this).balance;
     }
 
 }
- 
